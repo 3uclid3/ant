ab1b40f834af64eaa7f938380e04898deac0445b
3uclid3 <3uclid3@nubwork.dev>
2026-05-21 14:18:25 -0400

    feat(core): Allow compile-time disabling of assertions
    Introduces conditional compilation for the assertion mechanism, controlled by the
    `ANT_ASSERT_ENABLED` preprocessor define. This allows assertions to be completely
    removed from the build when `ANT_ASSERT_ENABLED` is set to 0.
    
    This functionality is immediately applied to the 'coverage' CMake preset, where
    assertions are now disabled.

--- src/core/assert.cpp
+++ src/core/assert.cpp
@@ -6,7 +6,8 @@
 // GCOV_EXCL_START
 namespace ant {
 namespace detail {
-
+#if ANT_ASSERT_ENABLED
+namespace {
 auto default_assert_handler(std::string_view condition_str, std::string_view message, std::source_location location) -> void
 {
     std::string full_message = "Assertion failed: ";
@@ -23,28 +24,29 @@ auto default_assert_handler(std::string_view condition_str, std::string_view mes
     std::abort();
 }
 
-static assert_handler& get_current_assert_handler()
+static assert_handler& get_handler()
 {
     static assert_handler handler = default_assert_handler;
     return handler;
 }
+} // namespace
+#endif
 
-auto invoke_assert_handler(std::string_view condition_str, std::string_view message, std::source_location location) -> void
+auto invoke_assert_handler(std::string_view condition_str [[maybe_unused]], std::string_view message [[maybe_unused]], std::source_location location [[maybe_unused]]) -> void
 {
-    get_current_assert_handler()(condition_str, message, location);
+    ANT_ASSERT_CODE(get_handler()(condition_str, message, location));
 }
 
 } // namespace detail
 
-auto set_assert_handler(assert_handler handler) -> void
+auto set_assert_handler(assert_handler handler [[maybe_unused]]) -> void
 {
-    detail::get_current_assert_handler() = std::move(handler);
+    ANT_ASSERT_CODE(detail::get_handler() = std::move(handler));
 }
 
 auto unset_assert_handler() -> void
 {
-    detail::get_current_assert_handler() = detail::default_assert_handler;
+    ANT_ASSERT_CODE(detail::get_handler() = detail::default_assert_handler);
 }
-
 } // namespace ant
 // GCOV_EXCL_STOP
